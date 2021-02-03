b2cb0f0ffb534835cd6e219b0d61944b8a62044f
Jiao Xianjun <xianjun.jiao@imec.be>
2021-02-03 15:56:18 +0100

    Update cmd.c

--- user_space/sdrctl_src/cmd.c
+++ user_space/sdrctl_src/cmd.c
@@ -1,4 +1,5 @@
-// SPDX-FileCopyrightText: 2019 Jiao Xianjun <putaoshu@msn.com>
+// Author: Xianjun Jiao
+// SPDX-FileCopyrightText: 2019 UGent
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 #include <stdbool.h>
