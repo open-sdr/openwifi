0c346c20061dad301b0af126991894cfaa8d0be1
Xianjun Jiao <xianjun.jiao@ugent.be>
2022-03-28 12:23:36 +0200

    Script to set lbt (listen before talk) threshold

---
+++ user_space/set_lbt_th.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -n $1 ]]; then
+  lbt_th=$1
+else
+  lbt_th=987654321 # no input
+fi
+
+set -x
+#set
+if [ $lbt_th -ne 987654321 ]; then
+  ./sdrctl dev sdr0 set reg drv_xpu 0 $lbt_th
+fi
+
+# show
+./sdrctl dev sdr0 get reg xpu 8 
+set +x
