afbf0d645c0a6bb259b4b3c7b74545d97fa796ae
Xianjun Jiao <xianjun.jiao@ugent.be>
2023-02-27 19:57:30 +0100

    Add disable script to support eifs trigger config

---
+++ user_space/eifs_by_last_rx_fail_disable.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+home_dir=$(pwd)
+
+if test -d "/sys/devices/platform/fpga-axi@0/fpga-axi@0:sdr"; then
+  cd /sys/devices/platform/fpga-axi@0/fpga-axi@0:sdr
+else 
+  cd /sys/devices/soc0/fpga-axi\@0/fpga-axi\@0\:sdr
+fi
+
+set -x
+#set
+if [[ -n $1 ]]; then
+  echo "4$1" > csma_cfg0
+fi
+
+# show
+cat csma_cfg0
+set +x
+
+cd $home_dir
