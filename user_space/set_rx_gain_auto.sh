75b4cf00fb09eb83e1ded6e728b17800f3137976
Xianjun Jiao <xianjun.jiao@ugent.be>
2025-03-13 13:38:33 +0100

    Update set_rx_gain_auto.sh by hacking ad9361 according: https://github.ugent.be/xjiao/openwifi/issues/148

--- user_space/set_rx_gain_auto.sh
+++ user_space/set_rx_gain_auto.sh
@@ -31,6 +31,29 @@ echo fast_attack > in_voltage0_gain_control_mode
 cat in_voltage0_gain_control_mode
 cat in_voltage0_hardwaregain
 
+if test -f "/sys/kernel/debug/iio/iio:device0/direct_reg_access"; then
+  cd /sys/kernel/debug/iio/device0/
+else if test -f "/sys/kernel/debug/iio/iio:device1/direct_reg_access"; then
+       cd /sys/kernel/debug/iio/iio:device1/
+     else if test -f "/sys/kernel/debug/iio/iio:device2/direct_reg_access"; then
+            cd /sys/kernel/debug/iio/iio:device2/
+          else if test -f "/sys/kernel/debug/iio/iio:device3/direct_reg_access"; then
+                 cd /sys/kernel/debug/iio/iio:device3/
+               else if test -f "/sys/kernel/debug/iio/iio:device4/direct_reg_access"; then
+                      cd /sys/kernel/debug/iio/iio:device4/
+                    else
+                      echo "Can not find direct_reg_access!"
+                      echo "Check log to make sure ad9361 driver is loaded!"
+                      exit 1
+                    fi
+               fi
+          fi
+     fi
+fi
+
+echo 0x0fa 0x5 > direct_reg_access
+echo 0x0fa 0xE5 > direct_reg_access
+
 cd $home_dir
 
 set +x
