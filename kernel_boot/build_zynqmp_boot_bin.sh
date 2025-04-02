7ee268f50551343a8b0afdd080d8b05e385a395e
Xianjun Jiao <xianjun.jiao@ugent.be>
2025-04-02 15:55:18 +0100

    Get new build_zynqmp_boot_bin.sh from: https://wiki.analog.com/resources/tools-software/linux-software/build-the-zynqmp-boot-image

--- kernel_boot/build_zynqmp_boot_bin.sh
+++ kernel_boot/build_zynqmp_boot_bin.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 set -ex
 
-HDF_FILE=$1
+XSA_FILE=$1
 UBOOT_FILE=$2
 ATF_FILE=${3:-download}
 BUILD_DIR=build_boot_bin
 OUTPUT_DIR=output_boot_bin
 
 usage () {
-	echo "usage: $0 system_top.<hdf/xsa> u-boot.elf  (download | bl31.elf | <path-to-arm-trusted-firmware-source>) [output-archive]"
+	echo "usage: $0 system_top.xsa u-boot.elf  (download | bl31.elf | <path-to-arm-trusted-firmware-source>) [output-archive]"
 	exit 1
 }
 
 depends () {
 	echo "Xilinx $1 must be installed and in your PATH"
-	echo "try: source /opt/Xilinx/Vivado/201x.x/settings64.sh"
+	echo "try: source /opt/Xilinx/Vivado/202x.x/settings64.sh"
 	exit 1
 }
 
 ### Check command line parameters
-echo $HDF_FILE | grep -q ".hdf\|.xsa" || usage
+echo $XSA_FILE | grep -q ".xsa" || usage
 echo $UBOOT_FILE | grep -q -e ".elf" -e "uboot" -e "u-boot" || usage
 
-if [ ! -f $HDF_FILE ]; then
-	echo $HDF_FILE: File not found!
+if [ ! -f $XSA_FILE ]; then
+	echo $XSA_FILE: File not found!
 	usage
-else
-	if [[ "$HDF_FILE" =~ ".hdf" ]]; then TOOL="xsdk";else TOOL="vitis";fi
 fi
 
 if [ ! -f $UBOOT_FILE ]; then
@@ -37,30 +35,30 @@ fi
 ### Check for required Xilinx tools (starting with 2019.2 there is no hsi anymore)
 command -v xsct >/dev/null 2>&1 || depends xsct
 command -v bootgen >/dev/null 2>&1 || depends bootgen
-if [[ "$HDF_FILE" =~ ".hdf" ]];then (command -v hsi >/dev/null 2>&1 || depends hsi);fi
 
 rm -Rf $BUILD_DIR $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 mkdir -p $BUILD_DIR
 
-# 2017.4 use 47af34b94a52b8cdc8abbac44b6f3ffab33a2206
-# 2018.1 use df4a7e97d57494c7d79de51b1e0e450d982cea98
-# 2018.2 use 93a69a5a3bc318027da4af5911124537f4907642
-# 2018.3 use 08560c36ea5b6f48b962cb4bd9a79b35bb3d95ce
-# 2019.3 use 713dace94b259845fd8eede11061fbd8f039011e
 # 2020.1 use bf72e4d494f3be10665b94c0e88766eb2096ef71
 # 2021.2 use 799131a3b063f6f24f87baa74e46906c076aebcd
+# 2022.2 use 5ebf70ea38e4626637568352b644acbffe3b13c1
+# 2023.1 use c7385e021c0b95a025f2c78384d57224e0120401
+# 2023.2 use 04013814718e870261f27256216cd7da3eda6a5d
 
-tool_version=$($TOOL -version | sed -n '3p' | cut -d' ' -f 3)
-if [ -z "$tool_version" ] ; then
-	echo "Could not determine Vivado version"
+tool_version=$(vitis -v | grep -o "Vitis v20[1-9][0-9]\.[0-9] (64-bit)" | grep -o "v20[1-9][0-9]\.[0-9]")
+if [[ "$tool_version" != "v20"[1-9][0-9]"."[0-9] ]] ; then
+	echo "Could not determine Vitis version"
 	exit 1
 fi
 atf_version=xilinx-$tool_version
 
 if [[ "$atf_version" == "xilinx-v2021.1" ]];then atf_version="xlnx_rebase_v2.4_2021.1";fi
 if [[ "$atf_version" == "xilinx-v2021.1.1" ]];then atf_version="xlnx_rebase_v2.4_2021.1_update1";fi
-if [[ "$atf_version" == "xilinx-v2021.2" ]];then atf_version="xlnx-v2021.2";fi
+if [[ "$atf_version" == "xilinx-v2021.2" ]];then atf_version="xilinx-v2021.2";fi
+if [[ "$atf_version" == "xilinx-v2022.2" ]];then atf_version="xilinx-v2022.2";fi
+if [[ "$atf_version" == "xilinx-v2023.1" ]];then atf_version="xilinx-v2023.1";fi
+if [[ "$atf_version" == "xilinx-v2023.2" ]];then atf_version="xilinx-v2023.2";fi
 
 if [[ "$4" == "uart1" ]];then console="cadence1";else console="cadence0";fi
 
@@ -93,52 +91,20 @@ else
 	cp $ATF_FILE $OUTPUT_DIR/bl31.elf
 fi
 
-cp "$HDF_FILE" "$BUILD_DIR/"
+cp "$XSA_FILE" "$BUILD_DIR/"
 cp "$UBOOT_FILE" "$OUTPUT_DIR/u-boot.elf"
-cp "$HDF_FILE" "$OUTPUT_DIR/"
+cp "$XSA_FILE" "$OUTPUT_DIR/"
 
-# Work-around for MPSoC ZCU102 and ZCU106 Evaluation Kits - DDR4 SODIMM change
-# (https://www.xilinx.com/support/answers/71961.html)
-if [ $tool_version == "v2018.3" ];then
-(
-	wget https://www.xilinx.com/Attachment/72113-files.zip -P $BUILD_DIR
-	unzip $BUILD_DIR/72113-files.zip -d  $BUILD_DIR
-)
-fi
 
 ### Create create_fsbl_project.tcl file used by xsct to create the fsbl.
-echo "hsi open_hw_design `basename $HDF_FILE`" > $BUILD_DIR/create_fsbl_project.tcl
+echo "hsi open_hw_design `basename $XSA_FILE`" > $BUILD_DIR/create_fsbl_project.tcl
 echo 'set cpu_name [lindex [hsi get_cells -filter {IP_TYPE==PROCESSOR}] 0]' >> $BUILD_DIR/create_fsbl_project.tcl
-### The fsbl creating flow is different starting with 2019.2 Xilinx version
-if [[ "$HDF_FILE" =~ ".hdf" ]];then
-        echo 'sdk setws ./build/sdk' >> $BUILD_DIR/create_fsbl_project.tcl
-        echo "sdk createhw -name hw_0 -hwspec `basename $HDF_FILE`" >> $BUILD_DIR/create_fsbl_project.tcl
-        echo 'sdk createapp -name fsbl -hwproject hw_0 -proc $cpu_name -os standalone -lang C -app {Zynq MP FSBL}' >> $BUILD_DIR/create_fsbl_project.tcl
-        echo 'configapp -app fsbl build-config release' >> $BUILD_DIR/create_fsbl_project.tcl
-	if [ $tool_version == "v2018.3" ];then
-		echo "file copy -force xfsbl_ddr_init.c ./build/sdk/fsbl/src" >> $BUILD_DIR/create_fsbl_project.tcl
-		echo "file copy -force xfsbl_hooks.c    ./build/sdk/fsbl/src" >> $BUILD_DIR/create_fsbl_project.tcl
-		echo "file copy -force xfsbl_hooks.h    ./build/sdk/fsbl/src" >> $BUILD_DIR/create_fsbl_project.tcl
-	fi
-	echo 'sdk projects -build -type all' >> $BUILD_DIR/create_fsbl_project.tcl
+echo 'platform create -name hw0 -hw system_top.xsa -os standalone -out ./build/sdk -proc $cpu_name' >> $BUILD_DIR/create_fsbl_project.tcl
+echo 'platform generate' >> $BUILD_DIR/create_fsbl_project.tcl
 
-	### Create create_pmufw_project.tcl
-	echo "set hwdsgn [open_hw_design `basename $HDF_FILE`]" > $BUILD_DIR/create_pmufw_project.tcl
-	echo 'generate_app -hw $hwdsgn -os standalone -proc psu_pmu_0 -app zynqmp_pmufw -compile -sw pmufw -dir pmufw' >> $BUILD_DIR/create_pmufw_project.tcl
-	echo 'quit' >> $BUILD_DIR/create_pmufw_project.tcl
-
-        FSBL_PATH="$BUILD_DIR/build/sdk/fsbl/Release/fsbl.elf"
-        SYSTEM_TOP_BIT_PATH="$BUILD_DIR/build/sdk/hw_0/system_top.bit"
-	PMUFW_PATH="$BUILD_DIR/pmufw/executable.elf"
-else
-	# Flow got changed starting with 2019.2 version (when Vitis replaced SDK) and pmufw is generated automatically with fsbl
-        echo 'platform create -name hw0 -hw system_top.xsa -os standalone -out ./build/sdk -proc $cpu_name' >> $BUILD_DIR/create_fsbl_project.tcl
-        echo 'platform generate' >> $BUILD_DIR/create_fsbl_project.tcl
-
-        FSBL_PATH="$BUILD_DIR/build/sdk/hw0/export/hw0/sw/hw0/boot/fsbl.elf"
-        SYSTEM_TOP_BIT_PATH="$BUILD_DIR/build/sdk/hw0/hw/system_top.bit"
-	PMUFW_PATH="$BUILD_DIR/build/sdk/hw0/export/hw0/sw/hw0/boot/pmufw.elf"
-fi
+FSBL_PATH="$BUILD_DIR/build/sdk/hw0/export/hw0/sw/hw0/boot/fsbl.elf"
+SYSTEM_TOP_BIT_PATH="$BUILD_DIR/build/sdk/hw0/hw/system_top.bit"
+PMUFW_PATH="$BUILD_DIR/build/sdk/hw0/export/hw0/sw/hw0/boot/pmufw.elf"
 
 ### Create zynq.bif file used by bootgen
 echo "the_ROM_image:" > $OUTPUT_DIR/zynq.bif
@@ -154,13 +120,6 @@ echo "}" >> $OUTPUT_DIR/zynq.bif
 (
 	cd $BUILD_DIR
 	xsct create_fsbl_project.tcl
-	if [[ "$HDF_FILE" =~ ".hdf" ]];then
-		hsi -source create_pmufw_project.tcl
-		### There was a bug in some vivado version where they build would fail -> check CC_FLAGS
-		grep "CC_FLAGS :=" pmufw/Makefile | grep -e "-Os" || sed -i '/-mxl-soft-mul/ s/$/ -Os -flto -ffat-lto-objects/' pmufw/Makefile
-		cd pmufw
-		make
-	fi
 )
 ### Copy fsbl and system_top.bit into the output folder
 cp "$FSBL_PATH" "$OUTPUT_DIR/fsbl.elf"
